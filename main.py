f034cd74e47684e4be3a47b74ed730085c09f54b
jonathan joly <41478399+joly534@users.noreply.github.com>
2021-03-31 21:41:47 +0200

    creation class
    creation de la classe objet page web, avec function show content on screen

--- main.py
+++ main.py
@@ -11,21 +11,32 @@ class PageWeb:
         self.url = url  
         http = urllib3.PoolManager()
         reponse = http.request('GET',self.url)
-        donnees = reponse.data
-        donnees.decode('utf-8')
+        self.donnees = reponse.data
+        self.donnees.decode('utf-8')
 
-    def readAndShowContent(self):
+    def parseContent(self):
+        pass
+
+    def drawContent(self):
+        pass
+
+    def followLink(self):
+        pass
+
+    def getSeo(self):
+        pass
+
+    def showContent(self):
         textScreenWindow.delete('1.0', "end")
-        textScreenWindow.insert(1.0,donnees)
+        textScreenWindow.insert(1.0,self.donnees)
         textScreenWindow.pack()
-        labelScreenWindow = tk.Label(screenWindow, text='Resultat de la requete pour ' + saisieUrl)
+        labelScreenWindow = tk.Label(screenWindow, text='Resultat de la requete pour ' + self.url)
         labelScreenWindow.pack()
 
-def analise(saisie_url):
-    print(saisie_url)
+def analise():
+    saisie_url = entry.get()
     pageWeb = PageWeb(saisie_url)
-    pageWeb.readAndShowContent()
-
+    pageWeb.showContent()
 
 
 ### FENETRE PRINCIPALE DU PROGRAMME
@@ -33,14 +44,16 @@ root = tk.Tk()
 root.geometry("720x600")
 root.title("Analiseur de site web")
 ### FENETRE CONTENANT L'ENTREE DE L'URL DU SITE A ANALISER ET LE RESULTAT DE LA REQUETTE
-url_window =tk.Frame(root)                 
+url_window =tk.Frame(root)
 url_window.pack()
-labelUrl= tk.Label (url_window, text="""Entrez l'URL du site à analiser""")           ### on crée une fenetre dans laquelle on affiche le status de la requette 
+### on crée une fenetre dans laquelle on affiche le status de la requette
+labelUrl= tk.Label (url_window, text="""Entrez l'URL du site à analiser""")
 labelUrl.pack()
-           ### on demande la saisie de l'url du site à analiser
-saisie_url = tk.Entry (url_window, width= 60)
-saisie_url.pack()
-bouton_url = tk.Button(url_window, text="Analyser", command=analise(saisie_url))      ### bouton pour lancer la requette
+### on demande la saisie de l'url du site à analiser
+entry = tk.Entry (url_window, width= 60)
+entry.pack()
+### bouton pour lancer la requette
+bouton_url = tk.Button(url_window, text="Analyser", command=analise)
 bouton_url.pack()
 ### FENETRE CONTENANT LE RESULTAT DE LA REQUETTE
 screenWindow = tk.Frame(root)
